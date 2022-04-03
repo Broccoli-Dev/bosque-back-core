1b4055b5976dfd59a3f37f0bc5dd280d677cd4e1
Santiago Osorio <saostero@gmail.com>
2022-04-03 12:52:20 -0500

    Docker was initialized

--- README.md
+++ README.md
@@ -71,3 +71,9 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+## Comandos Docker
+
+docker-compose up -d bosque_core
+docker-compose ps
+docker-compose down
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { .Controller } from './user.controller.ts/..controller';
+import { UserController } from './src/controllers/user/user.controller';
+import { UserController } from './controllers/user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, .Controller, UserController],
   providers: [AppService],
 })
 export class AppModule {}
